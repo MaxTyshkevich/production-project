929befefbf3e1432bfcc8d5d989144b914767016
Max Tyshkevich <2842778@gmail.com>
2024-09-03 14:27:42 +0300

    remove test redux

--- src/app/providers/StoreProvider/ui/StoreProvider.tsx
+++ src/app/providers/StoreProvider/ui/StoreProvider.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
 import { Provider } from 'react-redux';
-import { DeepPartial } from '@reduxjs/toolkit';
+
 import { createReduxStore } from '../config/store';
 import { StateSchema } from '../config/StateSchema';
 
 interface StoreProviderProps {
 
-    initialState?: DeepPartial<StateSchema>;
+    initialState?: any;
 }
 
 export const StoreProvider: FC<StoreProviderProps> = ({ children, initialState }) => {
